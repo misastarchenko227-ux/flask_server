04c52edb8c42dfb7e8a26e43bad51f8d60f6555e
misastarchenko227-ux <misastarchenko227@gmail.com>
2025-11-17 14:18:34 +0300

    Update server.py

--- server.py
+++ server.py
@@ -153,4 +153,4 @@ def list_users():
 if __name__ == '__main__':
     # 🔒 добавляем сертификаты
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug = True,host="0.0.0.0", port=port,use_reloader=True,reloader_type='auto',threaded=True)
